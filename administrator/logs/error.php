fb6718490e03301a2668d60adbadf3c9e6437210
Hernan Torres <hetorresp@gmail.com>
2016-12-29 07:43:26 -0500

    Creado-Base sin usuarios.
    Creación conceptobase sin adicinar link de usuarios.

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-12-27 01:48:19 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-12-27T01:48:19+00:00	INFO ::1	joomlafailure	El usuario y la clave no coinciden o no tiene una cuenta aún.
